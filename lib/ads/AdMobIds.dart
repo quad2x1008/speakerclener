a87e41b29197b0111229c1438302dd8c178c579b
Quad <himalayd.quad@gmail.com>
2024-04-05 09:57:19 +0530

    v1

--- lib/ads/AdMobIds.dart
+++ lib/ads/AdMobIds.dart
@@ -1,3 +1,12 @@
 /**
 
+
+    Speaker Cleaner Water eject(android - nktech)
+    com.simulator.speakerclean
+    App Open ca-app-pub-7400206081244674/9922897875
+    Banner ca-app-pub-7400206081244674/6785281412
+    Interstitial ca-app-pub-7400206081244674/3549061213
+    Native ca-app-pub-7400206081244674/2846036408
+    App ID ca-app-pub-7400206081244674~3703622815
+
  */
